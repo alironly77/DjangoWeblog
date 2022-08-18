4d657b27fe5060530acb1a2792a793c144ad6f22
alironly77 <aliebrahimipro@gmail.com>
2022-08-19 00:52:21 +0430

    Creat Home Page

--- articles_app/urls.py
+++ articles_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+app_name = 'articles_app'
 urlpatterns = [
-    path('', views.Articles),
+    path('', views.Articles,name="Home"),
+    path('<slug>', views.Articles_detail, name="detail"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- articles_app/views.py
+++ articles_app/views.py
@@ -1,10 +1,14 @@
 from datetime import date
 from django.shortcuts import render
 from . import models
+from django.http import HttpResponse
 
 
 def Articles(request):
     articles = models.Articles.objects.all().order_by('date')
     art = articles
-    return render(request, 'articles.html', {'art': art} )
- 
+    return render(request, 'articles.html', {'art': art})
+
+
+def Articles_detail(request, slug):
+    return HttpResponse(slug)
